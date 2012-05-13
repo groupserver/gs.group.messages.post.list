63c0824aa2cc0f8bd91b1f1c38185debcfe77c3a
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-14 09:26:20 +1000

    Initial import

---
+++ README.txt
@@ -0,0 +1,14 @@
+Introduction
+============
+
+Posts [#posts]_ made to a GroupServer group.
+
+..  Resources
+
+..  [#posts] Posts are displayed by the ``gs.group.messages.post`` 
+    product.
+..  [#topics] Topics are displayed by the ``gs.group.messages.topic`` 
+    product.
+..  [#group] The group page is provided by the ``gs.group.home`` 
+    product.
+
---
+++ setup.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+import os
+from setuptools import setup, find_packages
+from version import get_version
+
+version = get_version()
+
+setup(name='gs.group.messages.posts',
+    version=version,
+    description="The Posts tab in a GroupServer Group",
+    long_description=open("README.txt").read() + "\n" +
+                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
+      "Development Status :: 4 - Beta",
+      "Environment :: Web Environment",
+      "Framework :: Zope2",
+      "Intended Audience :: Developers",
+      "License :: Other/Proprietary License",
+      "Natural Language :: English",
+      "Operating System :: POSIX :: Linux"
+      "Programming Language :: Python",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+    keywords='groupserver message post',
+    author='Michael JasonSmith',
+    author_email='mpj17@onlinegroups.net',
+    url='http://groupserver.org/',
+    license='other',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'gs.content.js.jquery',
+        'gs.group.base',
+        'gs.group.home',
+        'gs.group.member.canpost',
+        'Products.GSGroup',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,)
+
