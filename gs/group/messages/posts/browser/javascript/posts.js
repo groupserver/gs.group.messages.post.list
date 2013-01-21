03da742cb521d2f9ca58ca855f6e2cba0cd1a683
Michael JasonSmith <mpj17@onlinegroups.net>
2013-01-22 10:56:50 +1100

    Trying to get the Posts to behave with the jQuery UI Tabs. Curse them.

--- gs/group/messages/posts/browser/javascript/posts.js
+++ gs/group/messages/posts/browser/javascript/posts.js
@@ -2,9 +2,17 @@ jQuery.noConflict();
 jQuery(document).ready( function () {
     var postsSearch = null;
     var show_posts = null;
+    var b = null;
+    var url = null;
+
+    b = jQuery('base').attr('href');
+    if (b[b.length -1] != '/') {
+      b = b + '/';
+    }
+    url = b + 'gs-group-messages-posts-ajax.html';
+
     postsSearch = GSSearch('#gs-group-messages-posts-search', 
-                           'gs-group-messages-posts-ajax.html', 
-                           0, 12, {}, null);
+                           url, 0, 12, {}, null);
     show_posts = function(event, ui) {
         // Tab counting starts at 0. Posts are the second tab.
         if ((ui.newPanel.attr('id') ===  'gs-group-messages-base-tabs-1') && 
