9188fffd272885a7c3066ce0a79b1aa79a95d204
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-11 23:38:03 +1100

    Switching to the gsJsLoader

--- gs/group/messages/posts/browser/javascript/posts.js
+++ gs/group/messages/posts/browser/javascript/posts.js
@@ -1,9 +1,10 @@
-jQuery.noConflict();
-jQuery(document).ready( function () {
+init_post_search = function () {
     var postsSearch = null;
     var show_posts = null;
     var b = null;
     var url = null;
+
+    jQuery.noConflict();
 
     b = jQuery('base').attr('href');
     if (b[b.length -1] != '/') {
@@ -18,5 +19,13 @@ jQuery(document).ready( function () {
             postsSearch.load();
         }
     };
-    jQuery('a[href="#gs-group-messages-base-tabs-1"]').on('shown', show_posts);
-});
+    jQuery('a[href="#gs-group-messages-base-tabs-1"]').on('click', show_posts);
+}
+
+
+window.addEventListener('load', function () {
+    gsJsLoader.with_modules(['/++resource++jquery-1.8.3.js',
+                             '/++resource++bootstrap-2.2.2/js/bootstrap.js',
+                             '/++resource++gs-search-base-js-20121217.js'],
+                            init_post_search);
+}, false);
