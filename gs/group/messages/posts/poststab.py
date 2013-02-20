e333da01014aa65e18bd30ccd42fbe1243eaa096
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-20 11:23:38 +1100

    Using a different base-class for the Posts viewlet, so the tal:condition is not needed

--- gs/group/messages/posts/poststab.py
+++ gs/group/messages/posts/poststab.py
@@ -3,12 +3,12 @@ from zope.cachedescriptors.property import Lazy
 from zope.component import getMultiAdapter
 from Products.GSGroup.interfaces import IGSMailingListInfo
 from gs.group.member.canpost.interfaces import IGSPostingUser
-from gs.group.home.simpletab import UserInfoTab
+from gs.group.home.simpletab import PublicTab
 
 
-class PostsTab(UserInfoTab):
+class PostsTab(PublicTab):
     def __init__(self, group, request, view, manager):
-        UserInfoTab.__init__(self, group, request, view, manager)
+        super(PostsTab, self).__init__(group, request, view, manager)
 
     @Lazy
     def canPost(self):
