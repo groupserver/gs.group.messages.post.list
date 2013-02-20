472daffea86d4a7fbab514d9c4b2227f38916b9a
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-20 11:50:40 +1100

    Cleaning up the Posts JS

--- gs/group/messages/posts/browser/javascript/posts.js
+++ gs/group/messages/posts/browser/javascript/posts.js
@@ -1,26 +1,29 @@
 jQuery.noConflict();
 
-function init_post_search() {
-    var postsSearch = null, show_posts = null, b = null, url = null;
+function gs_group_messages_posts_init_search() {
+    var postsSearch = null;
 
-    b = jQuery('base').attr('href');
-    if (b[b.length -1] != '/') {
-      b = b + '/';
-    }
-    url = b + 'gs-group-messages-posts-ajax.html';
-
-    postsSearch = GSSearch('#gs-group-messages-posts-search', 
-                           url, 0, 12, {}, null);
-    show_posts = function(event, ui) {
+    function posts_show (event, ui) {
         if (!postsSearch.results_shown()) {
             postsSearch.load();
         }
-    };
-    jQuery('a[href="#gs-group-messages-base-tabs-1"]').on('click', show_posts);
+    }
+    function get_url() {
+        var b = null, 
+        b = jQuery('base').attr('href');
+        if (b[b.length -1] != '/') {
+            b = b + '/';
+        }
+        return b + 'gs-group-messages-posts-ajax.html';
+    }
+    
+    postsSearch = GSSearch('#gs-group-messages-posts-search', 
+                           get_url(), 0, 12, {}, null);
+    jQuery('#gs-group-messages-base-tab-1 a').click(posts_show);
 }
 
 
 jQuery(window).load(function () {
     gsJsLoader.with_module('/++resource++gs-search-base-js-min-20121217.js',
-                           init_post_search);
+                           gs_group_messages_posts_init_search);
 });
